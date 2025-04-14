ad6034feb539d3ace0246c7bede04c22f3e8fa5a
Patricionrp <patricioriquelme330@gmail.com>
2025-04-14 17:55:34 -0300

    add: migrations and models for Spaces and Reservations

---
+++ backend/app/Models/Reservation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'space_id',
+        'reserved_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function space()
+    {
+        return $this->belongsTo(Space::class);
+    }
+}
