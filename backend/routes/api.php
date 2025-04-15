962a5ae0e20d3e59861a54696ba36f508973caac
Patricionrp <patricioriquelme330@gmail.com>
2025-04-15 02:22:48 -0300

    add: define user and admin routes for reservations and spaces in api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,14 +3,36 @@
 use App\Http\Controllers\Auth\AdminAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserAuthController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\SpaceController;
+
+/* USER ROUTES */
 
 Route::controller(UserAuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
     Route::post('/logout', 'logout')->middleware('auth:sanctum', 'role:user');
 });
+Route::middleware('auth:sanctum','role:user')->controller(ReservationController::class)->group(function () {
+    Route::post('/reservations', 'store');
+    Route::get('/user/reservations', 'getReservations');
+    Route::patch('/user/reservations/{reservation}/cancel', 'cancel');
+});
+Route::middleware(['auth:sanctum', 'role:user'])->controller(SpaceController::class)->group(function () {
+    Route::get('/spaces', 'index');
+    Route::get('/spaces/{space}', 'show');
+});
+
+/* ADMIN ROUTES */
 
 Route::controller(AdminAuthController::class)->group(function () {
     Route::post('/admin/login', 'login');
     Route::post('/admin/logout', 'logout')->middleware('auth:sanctum', 'role:admin');
 });
+
+Route::middleware(['auth:sanctum', 'role:admin'])->controller(ReservationController::class)->group(function () {
+    Route::get('/admin/reservations', 'getAllReservations');
+    Route::patch('/admin/reservations/{reservation}/approve', 'approve');
+    Route::patch('/admin/reservations/{reservation}/reject', 'reject');
+});
+
