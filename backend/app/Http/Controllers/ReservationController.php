d8eca35acf9a5fb360e649dec1a8bf66668c4d4a
Patricionrp <patricioriquelme330@gmail.com>
2025-04-15 02:22:23 -0300

    add: reservation approval and rejection methods, update status handling in ReservationController

--- backend/app/Http/Controllers/ReservationController.php
+++ backend/app/Http/Controllers/ReservationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ReservationRequest;
 use App\Models\Reservation;
 use App\Models\Space;
+use App\ReservationStatusEnum;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
@@ -12,25 +13,99 @@ class ReservationController extends Controller
 {
     use AuthorizesRequests;
 
-   public function store(ReservationRequest $request)
-   {
-       $validatedData = $request->validated();
+    
+    /**
+    * Store a new reservation
+    *
+    * @param ReservationRequest $request
+    * @return void
+    */
+    public function store(ReservationRequest $request)
+    {
+        $validatedData = $request->validated();
 
-       $space = Space::findOrFail($validatedData['space_id']);
+        $space = Space::findOrFail($validatedData['space_id']);
 
-       $this->authorize('reserve', $space); // SpacePolicy
+        $this->authorize('reserve', $space); // SpacePolicy
 
-       $reservation = Reservation::create([
-           'user_id' => Auth::id(),
-           'space_id' => $validatedData['space_id'],
-           'reserved_at' => $validatedData['reserved_at'],
-       ]);
+        $reservation = Reservation::create([
+            'user_id' => Auth::id(),
+            'space_id' => $validatedData['space_id'],
+            'reserved_at' => $validatedData['reserved_at'],
+            'status' => ReservationStatusEnum::RESERVED,
+        ]);
 
-       $space->update(['status' => 'reserved']);
+        $space->update(['status' => ReservationStatusEnum::RESERVED]);
 
-       return response()->json(['message' => 'Reservation created successfully.', 'reservation' => $reservation], 201);
-   }
+        return response()->json(['message' => 'Reservation created successfully.', 'reservation' => $reservation], 201);
+    }
 
+   /**
+    * Cancel a reservation
+    *
+    * @param Reservation $reservation
+    * @return void
+    */
+    public function cancel(Reservation $reservation)
+    {
+
+        $this->authorize('cancel', $reservation); // ReservationPolicy
+        $reservation->update(['status' => ReservationStatusEnum::CANCELLED]);
+
+        $space = Space::findOrFail($reservation->space_id);
+
+        $space->update(['status' => ReservationStatusEnum::AVAILABLE]);
+
+        return response()->json(['message' => 'Reservation cancelled successfully.']);
+    }
+   
+    /**
+     * Approve reservation method for admin
+     *
+     * @param Reservation $reservation
+     * @return void
+     */
+    public function approve(Reservation $reservation)
+    {
+        if ($reservation->status !== ReservationStatusEnum::RESERVED) {
+            return response()->json(['error' => 'Only reservations in the reserved state can be approved'], 422);
+        }
+       
+        $reservation->update([
+            'status' => ReservationStatusEnum::APPROVED,
+        ]);
+
+        $space = Space::findOrFail($reservation->space_id);
+        $space->update(['status' => ReservationStatusEnum::RESERVED]);
+       
+        return response()->json(['message' => 'Reservation approved.', 'reservation' => $reservation]);
+    }
+
+    /**
+     * Reject reservation method for admin
+     */
+
+    public function reject(Reservation $reservation)
+    {
+        if ($reservation->status !== ReservationStatusEnum::RESERVED) {
+            return response()->json(['error' => 'Only reservations in the reserved state can be rejected'], 422);
+        }
+
+        $reservation->update([
+            'status' => ReservationStatusEnum::REJECTED,
+        ]);
+        
+        $space = Space::findOrFail($reservation->space_id);
+        $space->update(['status' => ReservationStatusEnum::AVAILABLE]);
+        return response()->json(['message' => 'Reservation rejected.', 'reservation' => $reservation]);
+    }
+
+
+    /**
+    * Get all reservations for the authenticated user
+    *
+    * @return void
+    */
    public function getReservations()
    {
     $user = Auth::user();
@@ -38,5 +113,17 @@ class ReservationController extends Controller
         'reservations' => $user->reservations()->with('space')->get(),
     ]);
    }
+
+   /**
+    * Get all reservations for the admin
+    *
+    * @return void
+    */
+    public function getAllReservations()
+    {
+        return response()->json([
+            'reservations' => Reservation::with(['user', 'space'])->latest()->get()
+        ]);
+    }
 
 }
