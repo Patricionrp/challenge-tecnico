c34d12a90399b30cf7b2bb63373afb2fc8dbb738
Patricionrp <patricioriquelme330@gmail.com>
2025-04-16 02:29:00 -0300

    feat: update backend routes to get user data and allow admin to access user routes and operations

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,25 +5,34 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserAuthController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\SpaceController;
+use Illuminate\Http\Request;
 
 /* USER ROUTES */
+
+Route::get(('/user'),function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::controller(UserAuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
-    Route::post('/logout', 'logout')->middleware('auth:sanctum', 'role:user');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum', 'role:user,admin');
 });
-Route::middleware('auth:sanctum','role:user')->controller(ReservationController::class)->group(function () {
+Route::middleware('auth:sanctum','role:user,admin')->controller(ReservationController::class)->group(function () {
     Route::post('/reservations', 'store');
     Route::get('/user/reservations', 'getReservations');
     Route::patch('/user/reservations/{reservation}/cancel', 'cancel');
 });
-Route::middleware(['auth:sanctum', 'role:user'])->controller(SpaceController::class)->group(function () {
+Route::middleware(['auth:sanctum', 'role:user,admin'])->controller(SpaceController::class)->group(function () {
     Route::get('/spaces', 'index');
     Route::get('/spaces/{space}', 'show');
 });
 
 /* ADMIN ROUTES */
+
+Route::middleware(['auth:sanctum', 'role:admin'])->get('/admin/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::controller(AdminAuthController::class)->group(function () {
     Route::post('/admin/login', 'login');
